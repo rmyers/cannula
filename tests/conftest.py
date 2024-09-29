a0dbd04cac1f520d09c39d434e91475232050869
Robert Myers <rmyers@anaconda.com>
2024-09-29 13:01:42 -0500

    Fixing codegen and tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,14 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+LOG = logging.getLogger("cannula.tests")
+
+
+logging.basicConfig(
+    level=logging.DEBUG, filename="reports/test-report.log", filemode="w"
+)
+
+
+def pytest_runtest_setup(item):
+    LOG.info("=======================================================")
+    LOG.info(f"Running test: {item.name}")
+    LOG.info("=======================================================")
